9fc2f6108ca67402d37178b16e16ebdb75e9702a
aakash sharma <34199858+aakash140799@users.noreply.github.com>
2020-01-09 04:06:10 -0800

    loopback roles

---
+++ conFusionServerLoopback/server/boot/script.js
@@ -0,0 +1,34 @@
+
+module.exports = function(app) {
+    Customers = app.models.Customer;
+    Role = app.models.Role;
+    RoleMapping = app.models.RoleMapping;
+    
+    Customers.findOne({name:'Admin'})
+    .then((user) => {
+        if(!user){
+            var admin;
+            Customers.create({
+                username:'Admin', password:'password',email:'admin@conFusion.net'})
+            .then((user) => {
+                admin = user;
+                RoleMapping.destroyAll();
+                return Role.findOne({name:'admin'});
+            })
+            .then((role) => {
+                if(!role){return Role.create({name:'admin'});}
+                else{return Role.findOne({name:'admin'});}
+            })
+            .then((role) => {
+                return role.principals.create({
+                    principalType: RoleMapping.USER,
+                    principalId: admin[0].id
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                throw(err);
+            });
+        }
+    });
+}
